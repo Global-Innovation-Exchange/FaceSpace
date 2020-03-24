3616d18e39a8693cc428c01b5f56ef0fba5fb051
codes <codes@Codys-MacBook-Pro.local>
2020-03-23 17:24:40 -0700

    stars appear

--- function.js
+++ function.js
@@ -1,1 +1,8 @@
+import { mdiStarOutline, mdiStar } from '@mdi/js';
+
+console.log(mdiStarOutline); // M...Z Path data
+setInterval(function(){
+    document.querySelector('path').setAttribute('d', mdiStarOutline);
+},500) // TODO: remove this later and substitute with proper
+
 console.log('yo');
