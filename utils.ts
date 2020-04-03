3cb39cd912b14fef615538955ed97b1455351f54
Steven Guh <stevenguh@gmail.com>
2020-04-02 19:45:40 -0700

    Convert utils to ts

--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
-import * as workerTimers from 'worker-timers';
+import workerTimers from 'worker-timers';
 
-function sleep(ms) {
+function sleep(ms: number) {
     return new Promise((resolve) => workerTimers.setTimeout(resolve, ms));
   }
 
