e5e95e11677de96e87203e37f592071df1b385f8
Steven Guh <stevenguh@gmail.com>
2020-04-11 16:16:06 -0700

    Fix error in utils.ts

--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
-import workerTimers from 'worker-timers';
+import { setTimeout } from 'worker-timers';
 
 function sleep(ms: number) {
-    return new Promise((resolve) => workerTimers.setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
 export { sleep };
